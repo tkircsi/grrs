23209d26f7dd6aaebcc02f4c7f1f4377fc379b19
Tibor Kircsi <tkircsi@gmail.com>
2023-01-13 17:58:45 +0100

    add test

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ $ RUST_LOG=info ./target/debug/grrs --help
 [2023-01-13T13:09:36Z INFO  grrs] starting up
 Search for a pattern in a file and display the lines that contain it
 
-Usage: grrs.exe --pattern <PATTERN> --path <PATH>
+Usage: grrs --pattern <PATTERN> --path <PATH>
 
 Options:
       --pattern <PATTERN>  The pattern to look for
@@ -28,4 +28,15 @@ $ RUST_LOG=info ./target/debug/grrs --pattern let --path ./src/main.rs
     let buf = BufReader::new(f);
     // let content = std::fs::read_to_string(&args.path).expect("could not read file");
 [2023-01-13T13:08:51Z INFO  grrs] finished
+```
+## Run test
+```bash
+cargo test
+    Finished test [unoptimized + debuginfo] target(s) in 0.06s
+     Running unittests src/main.rs (target/debug/deps/grrs-fb81b795c67fca43)
+
+running 1 test
+test find_a_match ... ok
+
+test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use clap::Parser;
-use std::fs::File;
-use std::io::{BufReader, BufRead};
 use anyhow::{Context, Result};
-use log::{info, warn};
+use clap::Parser;
+use log::info;
+use std::fs::File;
+use std::io::BufReader;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser, Debug)]
@@ -15,37 +15,69 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     env_logger::init();
     info!("starting up");
 
     let args = Cli::parse();
     let path = &args.path.to_string_lossy();
+    let pattern = args.pattern;
 
     info!("open file {}", &path);
-    let f = File::open(&args.path)
-        .with_context(|| { format!("could not read file `{}`", path) })?;
-    let buf = BufReader::new(f); 
-
-
-    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let f = File::open(&args.path).with_context(|| format!("could not read file `{}`", path))?;
+    let buf = BufReader::new(f);
 
     info!("reading file...");
-    for line in buf.lines() {
-        match line {
-            Ok(l) => {
-                if l.contains(&args.pattern) {
-                    println!("{}", l);
-                }
-            },
-            Err(err) => {
-                panic!("{}", err);
-            }
-        }
-        
-    }
+    find_matches(buf, &pattern, &mut std::io::stdout()).expect("error reading file");
 
     info!("finished");
     Ok(())
+}
+
+fn find_matches(
+    reader: impl std::io::BufRead,
+    pattern: &str,
+    mut writer: impl std::io::Write,
+) -> Result<(), Box<dyn std::error::Error>> {
+    for line in reader.lines() {
+        match line {
+            Ok(l) => {
+                if l.contains(pattern) {
+                    writeln!(writer, "{}", l)?;
+                }
+            }
+            Err(err) => return Err(err.into()),
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    use stringreader::StringReader;
+
+    let input = r#"
+    [package]
+    name = "grrs"
+    version = "0.1.0"
+    edition = "2021"
+
+    # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+    [dependencies]
+    anyhow = "1.0.68"
+    clap = { version = "4.0.32", features = ["derive"] }
+    env_logger = "0.10.0"
+    log = "0.4.17"
+    "#;
+
+    let output = r#"    version = "0.1.0"
+    clap = { version = "4.0.32", features = ["derive"] }
+"#;
+    let string_reader = StringReader::new(input);
+    let reader = BufReader::new(string_reader);
+    let mut result = Vec::new();
+    find_matches(reader, "version", &mut result).expect("error reading file");
+
+    assert_eq!(result, output.as_bytes());
 }
